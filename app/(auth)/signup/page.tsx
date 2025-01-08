6d5b463c121d56d53f5ff790e24805d12496bde4
Ehiiz <Jaycass50@gmail.com>
2025-01-08 21:17:23 +0100

    `Update password validation in signup form to require at least one uppercase letter, one lowercase letter, one number and one special character.`

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -21,7 +21,10 @@ const schema = yup.object({
   lastName: yup.string().required("Last Name is required"),
   password: yup
     .string()
-    .min(8, "Password must be at least 8 characters long")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,
+      "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character"
+    )
     .required("Password is required"),
   privacy: yup.boolean().required().default(false),
 });
